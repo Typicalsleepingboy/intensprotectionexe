08fd92c9578c998bcb0178c50ed44cad59557e20
typicalsleepingboy <mpluto772@gmail.com>
2024-07-26 11:50:44 +0700

    first commit

---
+++ src/main/config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+  discordWebhookUrl: "https://discord.com/api/webhooks/1183773836185960589/DpFHYdKASvipa5uruw57ym_Qgs8w9-0r8uRA6S4-CG8Of4exApOGMvvx9MprovLGoGv-",
+  botAvatarUrl:
+    "https://cdn.discordapp.com/attachments/1185883650269650958/1264914897788080219/hq720_2.jpg?ex=669f9b4c&is=669e49cc&hm=0d7c981c1040845bbec36c4f0dc7a24c19d37d269e93346ea8b7acd608d25d7c&",
+  maintenanceMode: false,
+  port: 3000,
+};
---
+++ src/utils/theater.js
@@ -0,0 +1,90 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { v4: uuidv4 } = require('uuid');  // Import the uuid package
+
+const startingTheaterId = 2776;
+
+const fetchData = async () => {
+  const url = "https://jkt48.com/theater/schedule";
+  const result = await axios.get(url);
+  return result.data;
+};
+
+const parseData = (html) => {
+  const $ = cheerio.load(html);
+  const scheduleData = [];
+  
+  $("tbody tr").each((index, element) => {
+    const showInfoFull = $(element).find("td:nth-child(1)").html().trim();
+    const setlist = $(element).find("td:nth-child(2)").text().trim();
+    const members = $(element)
+      .find("td:nth-child(3) a")
+      .map((i, el) => $(el).text().trim())
+      .get();
+
+    const birthdayMembers = [];
+    const graduationIcons = [];
+
+    // Check for birthday members
+    $(element).find("td:nth-child(3)").children().each((i, el) => {
+      if ($(el).is('br') && $(el).next().is('img') && $(el).next().attr('src').includes('cat5.png')) {
+        $(el).next().nextAll('a').each((index, member) => {
+          birthdayMembers.push($(member).text().trim());
+        });
+      }
+      if ($(el).is('img') && $(el).attr('src').includes('cat7.png')) {
+        graduationIcons.push($(el).next('a').text().trim());
+      }
+    });
+
+    const { showInfo, date } = parseShowInfo(showInfoFull);
+
+    // Filter only the desired show data that has members
+    if (members.length > 0) {
+      scheduleData.push({
+        showInfo,
+        setlist,
+        members,
+        birthdayMembers,
+        graduationIcons,
+        date // Include the date for sorting
+      });
+    }
+  });
+
+  // Sort by date
+  scheduleData.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  // Reassign theater IDs starting from the earliest show date and add random IDs
+  const updatedScheduleData = scheduleData.map((data, index) => ({
+    ...data,
+    theaterId: startingTheaterId + index,
+    intensId: uuidv4()  // Add a random ID
+  }));
+
+  return updatedScheduleData;
+};
+
+const parseShowInfo = (showInfoFull) => {
+  const regex = /(\w+), (\d{1,2}\.\d{1,2}\.\d{4})<br>Show (\d{1,2}:\d{2})/;
+  const match = showInfoFull.match(regex);
+  let date, day, time;
+
+  if (match) {
+    day = match[1];
+    date = match[2];
+    time = match[3];
+    // Convert date to YYYY-MM-DD format for sorting
+    date = date.split('.').reverse().join('-');
+  } else {
+    // Handle case where regex does not match
+    date = showInfoFull.replace(/<br>/g, ' ').replace(/\s+/g, ' ').trim();
+  }
+
+  return {
+    showInfo: `${day ? day + ', ' : ''}${date ? date + ' ' : ''}${time || ''}`,
+    date
+  };
+};
+
+module.exports = { fetchData, parseData, parseShowInfo };
