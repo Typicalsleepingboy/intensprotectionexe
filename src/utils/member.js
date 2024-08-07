e7fa6c8135c753779a455b9133dc8cf2e63cba4e
Typicalsleepingboy.exe <99948517+Typicalsleepingboy@users.noreply.github.com>
2024-08-07 12:51:09 +0700

    Update member.js

--- src/utils/member.js
+++ src/utils/member.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const fetchMemberData = async () => {
-  const url = "https://jkt48.com/member/list?lang=id";
+  const url = "https://takagi.sousou-no-frieren.workers.dev/member/list?lang=id";
 
   try {
     const response = await axios.get(url);
