d77c59e8936667bdb7b72ff9455873fbffaf11db
Typicalsleepingboy.exe <99948517+Typicalsleepingboy@users.noreply.github.com>
2024-08-07 13:09:15 +0700

    Update memberid.js

--- src/utils/memberid.js
+++ src/utils/memberid.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 
 const fetchMemberDataId = async (memberId) => {
   try {
-    const response = await axios.get(`https://jkt48.com/member/detail/id/${memberId}?lang=id`);
+    const response = await axios.get(`https://takagi.sousou-no-frieren.workers.dev/detail/id/${memberId}?lang=id`);
     return response.data;
   } catch (error) {
     throw error;
